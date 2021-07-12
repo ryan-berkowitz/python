5789dbf1d7be0966bf3e5e678a3dccd2ccf35313
ryan-berkowitz <berkowitz.dev@gmail.com>
2021-07-12 07:11:29 -0400

    Added random number generator and error handling

--- Python Projects/Number Guessing Game.py
+++ Python Projects/Number Guessing Game.py
@@ -1,16 +1,21 @@
 #Returns winning number
 
-#Prompt entry of number
-print("Input number to guess")
-x = int(input())
+#Generate random number from 1-100
+import random
+x = random.randint(1,100)
 
 #Input Guesses
 guessList = []
 i = 'Y'
 while i != 'N':
-    print("Guess a number")
+    print("Guess a number between 1-100.")
     y = int(input())
-    guessList.append(y)
+    if y > 100:
+        print("Guess is greater than 100. Please input another guess.")
+    elif y < 1:
+        print("Guess is less than 1. Please input another guess.")
+    else:
+        guessList.append(y)
     print("Are there any more guesses? ('Y/N')?")
     i = input()
 
@@ -19,7 +24,15 @@ def sortList(n):
     return abs(n - x)
 
 #Decide Winner
-guessList.sort(key = sortList)
-winner = guessList[0]
-winningMsg = "The winner is {}"
-print(winningMsg.format(winner))
+if len(guessList) != 0:
+    guessList.sort(key = sortList)
+    winner = guessList[0]
+    listMsg = "The guesses were:"
+    print(listMsg)
+    print(guessList)
+    guessMsg = "The number to guess was {}."
+    print(guessMsg.format(x))
+    winningMsg = "The winning guess is {}."
+    print(winningMsg.format(winner))
+else:
+    print("No guesses were inputed.")
