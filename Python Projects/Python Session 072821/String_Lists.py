188d0875e75c095f6574d25b13d2448b133fca74
ryan-berkowitz <berkowitz.dev@gmail.com>
2021-08-05 13:42:55 -0400

    Added Andrew's solution

--- Python Projects/Python Session 072821/String_Lists.py
+++ Python Projects/Python Session 072821/String_Lists.py
@@ -1,7 +1,7 @@
 word = input("Enter a word: ")
 i=0
 g=0
-while i < len(word):
+while i <= len(word)//2:
     f = -i-1
     if word[i] == word[f]:
         i = i+1
@@ -10,6 +10,16 @@ while i < len(word):
         i = len(word)+1
 
 if g == 0:
+    print("Word is a palindrome")
+else:
+    print("Word is not a palindrome")
+
+#Andrew's solution
+word = input("Enter a word: ")
+
+m = [x for i, x in enumerate(word[:len(word)//2]) if word[i] != word[-i - 1]]
+
+if len(m) == 0:
     print("Word is a palindrome")
 else:
     print("Word is not a palindrome")
