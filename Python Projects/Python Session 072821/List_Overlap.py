d4a8ca0d364972f25baad438ed5065d996cd736e
ryan-berkowitz <berkowitz.dev@gmail.com>
2021-08-05 13:17:31 -0400

    Added random to List Overlap

--- Python Projects/Python Session 072821/List_Overlap.py
+++ Python Projects/Python Session 072821/List_Overlap.py
@@ -1,5 +1,7 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
+import random
+a = random.sample(range(1, 100), random.randint(0,10))
+print(a)
+b = random.sample(range(1, 100), random.randint(0,10))
+print(b)
 c = list(set([x for x in a if x in b]))
 print(c)
