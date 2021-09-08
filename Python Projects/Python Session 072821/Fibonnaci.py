76082b323b2f950aaab1972a9c5f290699224157
ryan-berkowitz <berkowitz.dev@gmail.com>
2021-09-08 14:58:53 -0400

    finished Fibonna

--- Python Projects/Python Session 072821/Fibonnaci.py
+++ Python Projects/Python Session 072821/Fibonnaci.py
@@ -1,11 +1,19 @@
 def fibonnaci(x):
     fib_num = [1]
     i = 1
-    while i < x:
-        y = fib_num[-1] + fib_num[-2]
-        fib_num.append[y]
-        i += 1
+    if x == 1:
+        return fib_num
+    else:
+        while i < x:
+            if len(fib_num) == 1:
+                y = fib_num[-1]
+                fib_num.append(y)
+            else:
+                y = fib_num[-1] + fib_num[-2]
+                fib_num.append(y)
+            i += 1
+        return fib_num
 
 z = int(input("Enter a number: "))
 
-fibonnaci(z)
+print(fibonnaci(z))
