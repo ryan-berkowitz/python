076711fa84d7a30bde2774d05ccf92d9a899a67a
ryan-berkowitz <berkowitz.dev@gmail.com>
2021-09-08 14:38:14 -0400

    Created Fibonnaci

---
+++ Python Projects/Python Session 072821/Fibonnaci.py
@@ -0,0 +1,11 @@
+def fibonnaci(x):
+    fib_num = [1]
+    i = 1
+    while i < x:
+        y = fib_num[-1] + fib_num[-2]
+        fib_num.append[y]
+        i += 1
+
+z = int(input("Enter a number: "))
+
+fibonnaci(z)
